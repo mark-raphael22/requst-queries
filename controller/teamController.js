a603ab2639aa5592220dc5f3a7a7a52df6f1162c
raphael347 <raphaelpaul1234>
2023-02-23 12:19:20 +0100

    footballApi

--- controller/teamController.js
+++ controller/teamController.js
@@ -37,7 +37,7 @@ const getAllTeams= async (req,res)=>{
             '>=': '$gte',
             '=': '$eq',
             '<': '$lt',
-            '<=': '$lte'
+            '<=': '$lte' 
         };
         const regEx = /\b(<|>|>=|<=|=)\b/g;
         let filters = numberFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express();
 const port=process.env.PORT || 9000;
 const mongoose=require('mongoose');
 const notfoundroute=require('./middleware/notfoundroute')
+const cors=require('cors');
 
 
 const teamRouter= require("./routes/teamRouter")
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(teamRouter);
 
