1845d0cb6ccba13e9134253ebd3215ed3210620c
raphael347 <raphaelpaul1234>
2023-02-23 12:35:26 +0100

    error route

--- middleware/notfoundroute.js
+++ middleware/notfoundroute.js
@@ -1,5 +1,5 @@
 const notfound=(req,res) => {
-    res.status(404).json({msg:"error"})
+    res.status(404).send(`Route not found try <a href='/api/v1/teams'>FOOTBALL API</a>`)
 }
 
 module.exports =notfound
